8ee2922d80f583fad33e49c2e43f6cdeafd19b24
bhepburn <brent.hepburn@gmail.com>
2025-01-12 15:12:58 +0000

    Switch to native graphite endpoint

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const net = require("net");
 
 const GRAPHITE_ENDPOINT = process.env.GRAPHITE_ENDPOINT;
-const GRAPHITE_TOKEN = process.env.GRAPHITE_TOKEN;
+const GRAPHITE_PORT = process.env.GRAPHITE_PORT;
 
 if (
   typeof GRAPHITE_ENDPOINT == "undefined" ||
-  typeof GRAPHITE_TOKEN == "undefined"
+  typeof GRAPHITE_PORT == "undefined"
 ) {
   console.error(
-    "Make sure environment variables (GRAPHITE_ENDPOINT & GRAPHITE_TOKEN) are set!"
+    "Make sure environment variables (GRAPHITE_ENDPOINT & GRAPHITE_PORT) are set!"
   );
   process.exit(1);
 }
@@ -23,64 +24,88 @@ const ns = (s) =>
     .toString()
     .toLowerCase()
     .replace(/[\W_]+/g, "_");
+
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+
 const generateMetricName = (state, zone, town, company) =>
   `neoil.prices.${ns(state)}.${ns(zone)}.${ns(town)}.${ns(company)}`;
 
-let promises = [];
-for (let ZONE = 1; ZONE <= ZONES; ZONE++) {
-  promises.push(
-    axios
-      .get(`https://www.newenglandoil.com/${STATE}/zone${ZONE}.asp?x=0`)
-      .then((response) => {
-        $ = cheerio.load(response.data);
+function sendMetricToGraphite(entry) {
+  return new Promise((resolve, reject) => {
+    const client = new net.Socket();
+    const message = `${entry.name} ${entry.value} ${entry.time}\n`;
 
-        const table = $("#oil-table > table");
+    console.log(message);
 
-        // Initialize an empty array to store the table data
-        const tableData = [];
+    client.connect(GRAPHITE_PORT, GRAPHITE_ENDPOINT, () => {
+      client.write(message, "utf8", () => {
+        client.end();
+        resolve();
+      });
+    });
 
-        // Loop over each row
-        table.find("tbody > tr").each((_, row) => {
-          const data = {};
-          // Establish each column, check to see if we ignore, otherwise push into object
-          $(row)
-            .find("td")
-            .each((_, column) => {
-              const dataLabel = $(column).attr("data-label").trim();
-              data[dataLabel] = $(column).text().trim();
-            });
-
-          // Set the time and push to data set
-          const entry = {
-            name: generateMetricName(
-              STATE,
-              ZONE,
-              data["Town"],
-              data["Company"]
-            ),
-            interval: 10,
-            time: TIME,
-            value: +data["Price"].replace("$", ""),
-          };
-          tableData.push(entry);
-        });
-
-        console.log(tableData);
-        return tableData;
-      })
-      // Push the data to Graphite service on Grafana
-      .then((tableData) =>
-        axios.post(GRAPHITE_ENDPOINT, tableData, {
-          headers: {
-            Authorization: "Bearer " + GRAPHITE_TOKEN,
-          },
-        })
-      )
-      .catch((err) => {
-        console.log("Fetch error " + err);
-        console.log(err.response);
-      })
-  );
+    client.on("error", (err) => {
+      reject(err);
+    });
+  });
 }
 
-Promise.all(promises);
+async function main() {
+  for (let ZONE = 1; ZONE <= ZONES; ZONE++) {
+    let promises = [];
+    promises.push(
+      axios
+        .get(`https://www.newenglandoil.com/${STATE}/zone${ZONE}.asp?x=0`)
+        .then((response) => {
+          $ = cheerio.load(response.data);
+
+          const table = $("#oil-table > table");
+
+          // Initialize an empty array to store the table data
+          const tableData = [];
+
+          // Loop over each row
+          table.find("tbody > tr").each((_, row) => {
+            const data = {};
+            // Establish each column, check to see if we ignore, otherwise push into object
+            $(row)
+              .find("td")
+              .each((_, column) => {
+                const dataLabel = $(column).attr("data-label").trim();
+                data[dataLabel] = $(column).text().trim();
+              });
+
+            // Set the time and push to data set
+            const entry = {
+              name: generateMetricName(
+                STATE,
+                ZONE,
+                data["Town"],
+                data["Company"]
+              ),
+              interval: 10,
+              time: TIME,
+              value: +data["Price"].replace("$", ""),
+            };
+            tableData.push(entry);
+          });
+
+          //console.log(tableData);
+          return tableData;
+        })
+        //Push the data to Graphite service on Grafana
+        .then((tableData) => {
+          return Promise.all(tableData.map(sendMetricToGraphite));
+        })
+        .catch((err) => {
+          console.log("Fetch error " + err);
+          console.log(err.response);
+        })
+    );
+    Promise.all(promises);
+    // Slow down the data load seems to cause problems if too many parallel
+    await sleep(10000);
+  }
+}
+
+main();
